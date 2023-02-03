a66f39cd58fef62de94d1375fde55aafa8a85aec
unorthodox06 <unorthodox06@gmail.com>
2023-02-03 18:54:06 +0530

    js

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+    let a ;
+    a = 10;
+
+    console.log(a);
+
+    var num = 200;
+    console.log(`the half of ${num} is ${num/2}`);
+
+    let l = 10;
+    console.log(l);
+
+    l = 19;
+    console.log(l);
