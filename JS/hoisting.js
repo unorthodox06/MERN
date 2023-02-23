92b2c30554f9673d1749df23213b6a1b3cff6580
unorthodox06 <unorthodox06@gmail.com>
2023-02-23 18:10:50 +0530

    diff done

---
+++ JS/hoisting.js
@@ -0,0 +1,16 @@
+console.log(a); //undefined 
+greet();
+console.log(hello);  
+hello();
+var a = 10;
+
+// function
+function greet() {
+  console.log("hello pepcoders");
+}
+//function expression 
+var hello = function () {
+    console.log("you guys rock");
+}
+
+// hello();
---
+++ JS/let,var,const.js
@@ -0,0 +1,6 @@
+console.log(a);
+var a = 10;
+// console.log(b);
+let b = 100;
+console.log(b);
+
